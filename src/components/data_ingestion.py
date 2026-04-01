431585eea3f8c0e57a1a6f34a9b3e7a73d1e9a84
Bossou Chiegueu Aryl Shamir <arylshamir98@gmail.com>
2026-04-01 05:51:32 +0100

    Hyperparamter Train

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from src.exceptions import CustomError
 from src.logger import logging
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 ARTIFACTS_DIR = "artifacts"
 
@@ -57,5 +58,8 @@ if __name__ == '__main__':
     train_data_path, test_data_path = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
         
--- src/utils.py
+++ src/utils.py
@@ -21,3 +21,32 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomError(e, sys)
+    
+def model_evaluation(X_train, y_train, X_test, y_test, models, params):
+    
+    try:
+        report = {}
+        
+        for model_name, model in models.items():
+           para = params[model_name]
+           
+           gs = GridSearchCV(model, para)
+           gs.fit(X_train, y_train)
+           
+           model.set_params(**gs.best_params_)
+           model.fit(X_train, y_train)
+           
+           y_train_pred = model.predict(X_train)
+           y_test_pred = model.predict(X_test)
+           
+           train_model_score = r2_score(y_train, y_train_pred)
+           test_model_score = r2_score(y_test, y_test_pred)
+           
+           report[model_name] = test_model_score
+           
+        return report
+           
+    except Exception as e:
+        raise CustomError
+
+    
